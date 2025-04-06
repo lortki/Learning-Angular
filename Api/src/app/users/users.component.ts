82fb39e999d3f8601640dc7b86b11db9516b4561
lortki <nikusha191208@gmail.com>
2025-04-06 15:58:23 +0400

    fix weird erorr

--- Api/src/app/users/users.component.ts
+++ Api/src/app/users/users.component.ts
@@ -3,6 +3,7 @@ import {User} from '../../models/user';
 import {UsersService} from '../../services/users/users.service';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-users',
